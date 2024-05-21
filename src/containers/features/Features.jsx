7aa18f615ea90d9146a4e5efa4c8449ed1fe9daa
Yidne <Yamiyekn@gmail.com>
2024-05-21 08:15:15 +0300

    Create Blog, Features, Header, Possibility, WhatGPT3 and Footer components structure.

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./features.css";
+
+const Features = () => {
+	return <div>Features</div>;
+};
+
+export default Features;
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.css";
+
+const Headers = () => {
+	return <div>Headers</div>;
+};
+
+export default Headers;
