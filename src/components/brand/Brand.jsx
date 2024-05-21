36c281e0c5b84287c8f91ab635d315e7d12d9925
Yidne <Yamiyekn@gmail.com>
2024-05-21 08:12:40 +0300

    Create Article, Brand, CTA, Feature, Navbar components and styled them.

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./brand.css";
+
+const Brand = () => {
+	return <div>Brand</div>;
+};
+
+export default Brand;
---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./cta.css";
+const CTA = () => {
+	return <div>Cta</div>;
+};
+
+export default CTA;
